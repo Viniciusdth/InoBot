15f2205de1d9c511a608a04f5d09c77732b9f316
Vinicius Botelho <vi.f.botelho@gmail.com>
2021-11-08 10:03:21 -0300

    store react-redux working with Increment/Decrement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+// let stats = { value: 1 };
+// let actions = {actionIncrement : "INCREMENT", actionDecrement: "DECREMENT"}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <p>teste</p>
+      {/* <Counter state={stats, actions} /> */}
+    </section>
   );
 }
 
